73eee6e3522ef89afee92d328a2523507880ce0f
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-06-07 15:13:04 +0300

    Update connection1.php

--- connection1.php
+++ connection1.php
@@ -1,5 +1,4 @@
 <?php
-
 /* Attempt MySQL server connection. Assuming you are running 
 MySQL server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root","", "Php_connection");
