ee197ded14e685333c0fcb01a12f251da6acde08
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-06-22 00:03:38 +0300

    Update Link_db.php

--- Link_db.php
+++ Link_db.php
@@ -3,3 +3,7 @@ $link= msqli_connect ("localhost","root","","dbtrail");
 if($link===false){
   die("EROR: Couldn't connect".conn_eror());
 }
+else{
+
+  echo"connection succesfull".get_host_ìnfo();
+}
