accaf4770dbec9cf451eaa27b97fa9e837c1dab4
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-07-06 14:51:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function validation() {
     } else if (document.Formfill.Email.value == "") {
         document.getElementById("result").innerHTML = "Enter your Email*";
         return false;
-<
+
     } else if (document.Formfill.Password.value == "") {
         document.getElementById("result").innerHTML = "Enter your Password*";
         return false;
