a486c8073447eeccbabe0f3aab9c92f7c0846e60
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-07-30 14:35:13 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -20,7 +20,7 @@ if($link === false)
 die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 // Attempt create database query execution
-$sql = "CREATE DATABASE For1";
+$sql = "CREATE DATABASE Form1";
 if(mysqli_query($link, $sql))
 {
 echo "Database created successfully";
